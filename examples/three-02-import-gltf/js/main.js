60b9a4bba93d042092031bd9a6b73bcf2d17c929
larkbuck <lark.buckingham@sjsu.edu>
2024-03-22 10:25:41 -0700

    Imported glTF tutorial code

--- examples/three-02-import-gltf/js/main.js
+++ examples/three-02-import-gltf/js/main.js
@@ -1,5 +1,12 @@
-// Basic Three.JS scene from documentation, importing Three.JS through a CDN 
-// https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
+/* 
+glTF import:
+- glTF loader imported + enabled
+-Global variable added to store dog gltf
+-Two directional lights added to view glTF
+- Added HELPERS to debug light position (disable after you place them)
+-glTF imported from blender (not it is an *embedded* .glTF file, not .glb)
+-Changed material on cube from BASIC to STANDARD so that the geometry catches light
+*/
 
 
 //~~~~~~~Import Three.js (also linked to as an import map in the HTML)~~~~~~
@@ -13,7 +20,7 @@ import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders
 
 
 // ~~~~~~~~~~~~~~~~ Declare Global Variables~~~~~~~~~~~~~~~~
-let scene, camera, renderer, cube;
+let scene, camera, renderer, cube, dog;
 
 
 // ~~~~~~~~~~~~~~~~ Initialize Scene in init() ~~~~~~~~~~~~~~~~
@@ -30,15 +37,27 @@ function init() {
 
 
     // ~~~~~~ Add Lights ~~~~~~
+    // Add helpers to debug the lights' position - COMMENT OUT WHEN DONE placing the light! https://threejs.org/docs/#api/en/helpers/DirectionalLightHelper
 
     // ~~ add directional light 
-    const light = new THREE.DirectionalLight(0xffffff, 3);
-    light.position.set(3, 4, 5);
-    scene.add(light);
+    const lightRight = new THREE.DirectionalLight(0xffffff, 3);
+    lightRight.position.set(3, 4, 5);
+    scene.add(lightRight);
 
-    // Add helper to debug the light's position - COMMENT OUT WHEN DONE placing the light! https://threejs.org/docs/#api/en/helpers/DirectionalLightHelper
-    // const helper = new THREE.DirectionalLightHelper(light, 5);
-    // scene.add(helper);
+    const helperRight = new THREE.DirectionalLightHelper(lightRight, 5);
+    scene.add(helperRight); // comment out when done placing light
+
+
+    // ~~ add directional light 
+    const lightLeft = new THREE.DirectionalLight(0xff00000, 3);
+    lightLeft.position.set(-3, 4, 5);
+    scene.add(lightLeft);
+
+    const helperLeft = new THREE.DirectionalLightHelper(lightLeft, 5);
+    scene.add(helperLeft); // comment out when done placing light
+
+
+
 
 
 
@@ -53,11 +72,13 @@ function init() {
 
     // ---> create cube
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+    // -> change material from Basic to standard for geometry to capture lights
+    // const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 
     const texture = new THREE.TextureLoader().load('textures/animalPrint-crop-512.png');
 
-    const material = new THREE.MeshBasicMaterial({ map: texture });
+    const material = new THREE.MeshStandardMaterial({ map: texture });
     // texture.minFilter = THREE.LinearFilter; // makes image sharper but aliased
 
     cube = new THREE.Mesh(geometry, material);
@@ -68,8 +89,10 @@ function init() {
 
     // load dog model
     loader.load('assets/dog_shiny.gltf', function (gltf) {
-        const dog = gltf.scene;
+        dog = gltf.scene;
         scene.add(dog);
+        dog.scale.set(2, 2, 2); // scale your model
+        dog.position.y = -2; // set initial position
     });
 
 
@@ -91,10 +114,13 @@ function animate() {
 
     // →→→→→→ add your animation here ↓↓↓↓
 
-    // camera.position.z += .03;
     cube.rotation.x += 0.007;
     cube.rotation.y += 0.007;
 
+    if (dog) {
+        // dog.rotation.x += 0.007;
+        dog.rotation.y += 0.007;
+    }
 
 
     // always end animation loop with renderer
@@ -112,3 +138,4 @@ window.addEventListener('resize', onWindowResize, false);
 
 init(); // execute initialize function
 animate(); // execute animation function
+
